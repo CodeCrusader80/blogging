e2e521acc70dd6e8e18cdb3166e68236e353f0a2
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-04 17:09:03 +0200

    Add social logo for navbar

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import s from "./navbar.module.css";
+import Image from "next/image";
 const Navbar = () => {
     return(
-        <div className={s.container}>Navbar</div>
+        <div className={s.container}>
+            <div className={s.social}>
+                <Image src={"/facebook.svg"} alt={"facebook logo"} width={24} height={24}/>
+                <Image src={"/linkedin.svg"} alt={"LinkedIn logo"} width={23} height={23}/>
+                <Image src={"/twitter-x-logo.svg"} alt={"Twitter logo"} width={22} height={22}/>
+                <Image src={"/youtube.svg"} alt={"facebook logo"} width={25} height={25}/>
+            </div>
+            <div className={s.logo}></div>
+            <div className={s.links}></div>
+        </div>
     )
 }
 export default Navbar;
