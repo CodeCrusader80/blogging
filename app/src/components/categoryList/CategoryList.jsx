0ea105e69f50bdb6b06b9b90ce94d93e10341b3e
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-06 16:55:23 +0200

    Add the base of CategoryList
    Now I just have to style it all.

--- app/src/components/categoryList/CategoryList.jsx
+++ app/src/components/categoryList/CategoryList.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import s from "./categorylist.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const CategoryList = () => {
     return(
-        <div className={s.container}>Navbar</div>
+        <div className={s.container}>
+            <h1>Popular Categories</h1>
+            <div className={s.categories}>
+                <Link href={"/bolg?cat=style"} className={`${s.category} ${s.style}`}>
+                    <Image src={"/lifestyle.png"} alt={"illustration style"} width={32} height={32} className={s.image}></Image>
+                    Style
+                </Link>
+                <Link href={"/bolg?cat=fashion"} className={`${s.category} ${s.fashion}`}>
+                    <Image src={"/style.png"} alt={"illustration style"} width={32} height={32} className={s.image}></Image>
+                    Fashion
+                </Link>
+                <Link href={"/bolg?cat=culture"} className={`${s.category} ${s.culture}`}>
+                    <Image src={"/culture.png"} alt={"illustration style"} width={32} height={32} className={s.image}></Image>
+                    Culture
+                </Link>
+                <Link href={"/bolg?cat=food"} className={`${s.category} ${s.food}`}>
+                    <Image src={"/food.png"} alt={"illustration style"} width={32} height={32} className={s.image}></Image>
+                   Food
+                </Link>
+                <Link href={"/bolg?cat=travel"} className={`${s.category} ${s.travel}`}>
+                    <Image src={"/travel.png"} alt={"illustration style"} width={32} height={32} className={s.image}></Image>
+                    Travel
+                </Link>
+                <Link href={"/bolg?cat=programming"} className={`${s.category} ${s.programming}`}>
+                    <Image src={"/programming.png"} alt={"illustration style"} width={32} height={32} className={s.image}></Image>
+                    Programming
+                </Link>
+            </div>
+        </div>
     )
 }
 export default CategoryList;
