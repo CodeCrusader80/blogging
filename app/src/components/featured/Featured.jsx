0584962f2399477442276f05e20537fe7e364f27
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-06 15:10:56 +0200

    Featured Post done

--- app/src/components/featured/Featured.jsx
+++ app/src/components/featured/Featured.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./featured.module.css";
+import Image from "next/image";
 const Featured = () => {
     return(
         <div className={s.container}>
@@ -8,9 +9,13 @@ const Featured = () => {
             </h1>
             <div className={s.post}>
                 <div className={s.imgContainer}>
-
+                    <Image src={"/p1.png"} alt={"illustration of a futuristic city"} height={320} width={450} className={s.image}/>
                 </div>
-                <div className={s.txtContainer}></div>
+                <div className={s.txtContainer}>
+                    <h1 className={s.fPostTitle}>Lorem ipsum dolor sit amet, consectetur.</h1>
+                    <p className={s.fPostDesc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam earum, eum molestias tenetur vel.</p>
+                    <button className={s.button}>Read More</button>
+                </div>
             </div>
         </div>
     )
