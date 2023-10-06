91db0c1fa12585a4e73ca4fb61687d793227a969
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-06 11:29:55 +0200

    Start feature part of the homepage

--- app/src/components/featured/Featured.jsx
+++ app/src/components/featured/Featured.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import s from "./featured.module.css";
 const Featured = () => {
     return(
-        <div className={s.container}>Navbar</div>
+        <div className={s.container}>
+            <h1 className={s.title}>
+                <b>Hey devs!</b> Discover our stories and creative ideas for your work.
+            </h1>
+            <div className={s.post}>
+                <div className={s.imgContainer}>
+                    
+                </div>
+                <div className={s.txtContainer}></div>
+            </div>
+        </div>
     )
 }
 export default Featured;
