38bfed12a0813e8736f729a5080c30d421d061cf
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-08 10:30:13 +0200

    Start Posts list and right menu

--- app/src/components/cardList/CardList.jsx
+++ app/src/components/cardList/CardList.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import s from "./cardlist.module.css";
 import Pagination from "@/app/src/components/pagination/Pagination";
+import Image from "next/image";
 const CardList = () => {
     return(
-        <div className={s.container}>Navbar
+        <div className={s.container}>
+            <h1 className={s.title}>Recent Posts</h1>
+            <div className={s.posts}>
+                <div className={s.post}>
+                    <div className={s.imgContainer}>
+                       <Image src={"/p2.png"} alt={"photo of a woman in a cyberpunk universe"} />
+                    </div>
+                    <div className={s.txtContainer}>
+                        
+                    </div>
+                </div>
+            </div>
         <Pagination/>
         </div>
     )
