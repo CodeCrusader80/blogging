89c06b00234a2a39b9ffccda078fb08907499737
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-09 10:20:40 +0200

    Create Card for the post section

---
+++ app/src/components/card/Card.jsx
@@ -0,0 +1,26 @@
+import s from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Card = () => {
+    return (
+        <div className={s.container}>
+            <div className={s.imgContainer}>
+                <Image src={"/p2.png"} alt={"photo of a woman in a cyberpunk universe"} fill={true}/>
+            </div>
+            <div className={s.txtContainer}>
+                <div className={s.detail}>
+                    <span className={s.date}>09/10/2023</span>
+                    <span className={s.cat}>Code</span>
+                </div>
+                <Link href={"/"}>
+                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                </Link>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cumque delectus, deserunt illo porro tenetur...</p>
+                <Link href={"/"}>Read More</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- app/src/components/cardList/CardList.jsx
+++ app/src/components/cardList/CardList.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import s from "./cardlist.module.css";
 import Pagination from "@/app/src/components/pagination/Pagination";
 import Image from "next/image";
+import Card from "@/app/src/components/card/Card";
 const CardList = () => {
     return(
         <div className={s.container}>
             <h1 className={s.title}>Recent Posts</h1>
             <div className={s.posts}>
                 <div className={s.post}>
-                    <div className={s.imgContainer}>
-                       <Image src={"/p2.png"} alt={"photo of a woman in a cyberpunk universe"} />
-                    </div>
-                    <div className={s.txtContainer}>
-                        
-                    </div>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
                 </div>
             </div>
         <Pagination/>
