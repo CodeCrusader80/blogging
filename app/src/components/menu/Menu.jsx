ce126b49dc6a5e2a42e6a23d55d2c5aff146fd29
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-09 16:18:15 +0200

    Side menu is nearly done

--- app/src/components/menu/Menu.jsx
+++ app/src/components/menu/Menu.jsx
@@ -5,15 +5,67 @@ import Image from "next/image";
 const Menu = () => {
     return(
         <div className={s.container}>
-            <h2>What's hot ?</h2>
-            <h1>Most Popular</h1>
+            <h2 className={s.subtitle}>{"What's hot ?"}</h2>
+            <h1 className={s.title}>Most Popular</h1>
             <div className={s.items}>
                 <Link href={"/"} className={s.item}>
                     <div className={s.imgContainer}>
-                        <Image src={"/p3.png"} alt={"highly detailed female droid"} width={300} height={180}/>
+                        <Image src={"/p3.png"} alt={"highly detailed female droid"} fill={true} className={s.img}/>
                     </div>
                     <div className={s.txtContainer}>
-
+                        <span className={`${s.category} ${s.travel}`}>Travel</span>
+                        <h3 className={s.titlePost}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </h3>
+                        <div className={s.details}>
+                            <span className={s.author}>John Doe </span>
+                            <span className={s.date}> 09/10/2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={s.item}>
+                    <div className={s.imgContainer}>
+                        <Image src={"/p3.png"} alt={"highly detailed female droid"} fill={true} className={s.img}/>
+                    </div>
+                    <div className={s.txtContainer}>
+                        <span className={`${s.category} ${s.style}`}>Style</span>
+                        <h3 className={s.titlePost}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </h3>
+                        <div className={s.details}>
+                            <span className={s.author}>John Doe </span>
+                            <span className={s.date}> 09/10/2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={s.item}>
+                    <div className={s.imgContainer}>
+                        <Image src={"/p3.png"} alt={"highly detailed female droid"} fill={true} className={s.img}/>
+                    </div>
+                    <div className={s.txtContainer}>
+                        <span className={`${s.category} ${s.fashion}`}>Fashion</span>
+                        <h3 className={s.titlePost}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </h3>
+                        <div className={s.details}>
+                            <span className={s.author}>John Doe </span>
+                            <span className={s.date}> 09/10/2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href={"/"} className={s.item}>
+                    <div className={s.imgContainer}>
+                        <Image src={"/p3.png"} alt={"highly detailed female droid"} fill={true} className={s.img}/>
+                    </div>
+                    <div className={s.txtContainer}>
+                        <span className={`${s.category} ${s.code}`}>Code</span>
+                        <h3 className={s.titlePost}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </h3>
+                        <div className={s.details}>
+                            <span className={s.author}>John Doe </span>
+                            <span className={s.date}> 09/10/2023</span>
+                        </div>
                     </div>
                 </Link>
             </div>
