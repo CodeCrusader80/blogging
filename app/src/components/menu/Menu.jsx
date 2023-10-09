7239d65543b288ad047281c7c8e7ff3ada73d0a6
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-09 11:37:58 +0200

    Start menu section

--- app/src/components/menu/Menu.jsx
+++ app/src/components/menu/Menu.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import s from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const Menu = () => {
     return(
-        <div className={s.container}>Navbar</div>
+        <div className={s.container}>
+            <h2>What's hot ?</h2>
+            <h1>Most Popular</h1>
+            <div className={s.items}>
+                <Link href={"/"} className={s.item}>
+                    <div className={s.imgContainer}>
+                        <Image src={"/p3.png"} alt={"highly detailed female droid"} width={200} height={150}/>
+                    </div>
+                    <div className={s.txtContainer}>
+
+                    </div>
+                </Link>
+            </div>
+        </div>
     )
 }
 export default Menu;
