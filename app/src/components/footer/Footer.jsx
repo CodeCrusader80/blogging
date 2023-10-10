4a6df08d3cab77ec9822ac1ca5db5d35c8f170da
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-10 10:38:35 +0200

    Start footer

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import s from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
     return(
-        <div className={s.container}>Footer</div>
+        <div className={s.container}>
+            <div className={s.info}>
+                <div className={s.logo}>
+                    <Image src={"/logo.png"} alt={"logo"} width={50} height={50}></Image>
+                </div>
+            </div>
+            <div className={s.links}></div>
+        </div>
     )
 }
 export default Footer;
