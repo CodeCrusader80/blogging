e62cfba54e2f34ae73ee6e9d28fa26ab0f4bf356
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-10 10:14:05 +0200

    pagination done

--- app/src/components/pagination/Pagination.jsx
+++ app/src/components/pagination/Pagination.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import s from "./pagination.module.css";
 const Pagination = () => {
     return(
-        <div className={s.container}>Navbar</div>
+        <div className={s.container}>
+            <button className={s.button}>Previous</button>
+            <button className={s.button}>Next</button>
+        </div>
     )
 }
 export default Pagination;
