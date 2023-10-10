efc5129d0fdf80a2771c657dd7d9d4bdfe42c54a
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-10 12:11:10 +0200

    put the links, logo and desc in footer

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./footer.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
     return(
@@ -8,9 +9,25 @@ const Footer = () => {
             <div className={s.info}>
                 <div className={s.logo}>
                     <Image src={"/logo.png"} alt={"logo"} width={50} height={50}></Image>
+                    <h1 className={s.logoTxt}>Blogging</h1>
+                </div>
+                <p className={s.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet consequuntur cupiditate exercitationem harum laboriosam perspiciatis praesentium sequi ut veritatis?</p>
+                <div className={s.icons}>
+                    <Image src={"/facebook.svg"} alt={"logo facebook"} width={18} height={18}/>
+                    <Image src={"/twitter-x-logo.svg"} alt={"twitter logo"} width={18} height={18}/>
+                    <Image src={"/linkedin.svg"} alt={"linkedin logo"} width={18} height={18}/>
+                    <Image src={"/youtube.svg"} alt={"youtube logo"} width={18} height={18}/>
                 </div>
             </div>
-            <div className={s.links}></div>
+            <div className={s.links}>
+                <div className={s.list}>
+                    <span className={s.listTitle}>Links</span>
+                    <Link href={"/"}>Home</Link>
+                    <Link href={"/"}>Blog</Link>
+                    <Link href={"/"}>About</Link>
+                    <Link href={"/"}>Contact</Link>
+                </div>
+            </div>
         </div>
     )
 }
