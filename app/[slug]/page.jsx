867046b3e1e2e6bf6f79693bc0f71c73feabcfa3
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-11 18:03:24 +0200

    CSS of the page post

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -7,7 +7,7 @@ const SinglePage = () => {
         <div className={s.container}>
             <div className={s.infoContainer}>
                 <div className={s.txtContainer}>
-                    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                    <h1 className={s.title}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
                     <div className={s.user}>
                         <div className={s.userImgContainer}>
                             <Image src={"/p2.png"} alt={""} fill className={s.userImg}/>
@@ -23,7 +23,13 @@ const SinglePage = () => {
                 </div>
             </div>
             <div className={s.content}>
-                <div className={s.post}></div>
+                <div className={s.description}>
+                    <div className={s.post}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut culpa cupiditate dolore doloremque earum eos esse harum hic in inventore, odit quasi quidem quis quod similique veniam veritatis vero!</p>
+                        <h5>Lorem ipsum dolor.</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti eligendi officia porro quia ullam! Deleniti doloribus facere neque quia quibusdam quidem quo vel veniam voluptatum. Amet atque esse ex excepturi exercitationem explicabo, impedit in numquam odit perferendis quidem repellendus tenetur.</p>
+                    </div>
+                </div>
                 <Menu/>
             </div>
         </div>
