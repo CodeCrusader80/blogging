f5c4da596a472839c1c7faf0b686f0e4e02545f7
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-12 14:35:09 +0200

    Update comments section stay at use CSS

--- app/src/components/comments/Comments.jsx
+++ app/src/components/comments/Comments.jsx
@@ -1,9 +1,33 @@
-import s from "comments.module.css";
+import s from "./comments.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Comments = () => {
+    const status = "authenticated"
     return(
         <div className={s.container}>
-            
+            <h1 className={s.title}>Comments</h1>
+            {status === "authenticated" ? (
+                    <div className={s.write}>
+                        <textarea placeholder={"write a comment..."} className={s.input}/>
+                        <button className={s.button}>Send</button>
+                    </div>
+            ) :
+            (
+                <Link href={"/login"}>Login to write a comment</Link>
+            )
+            }
+            <div className={s.comments}>
+                <div className={s.comment}>
+                    <div className={s.user}>
+                        <Image src={"/p2.png"} alt={"user avatar"} width={50} height={50}/>
+                        <div className={s.userInfo}>
+                            <span className={s.username}>John Doe</span>
+                            <span className={s.date}>12/10/2023</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
