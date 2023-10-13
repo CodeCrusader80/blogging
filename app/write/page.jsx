b3eda3570737de11db36d9c382615d74dfdd681a
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-13 16:36:58 +0200

    Add svg and start structure of writing page

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,9 +1,29 @@
 import s from "./writePage.module.css";
+import Image from "next/image";
+import {useState} from "react";
 
 const WritePage = () => {
+
+    const [open, setOpen] = useState(false)
     return (
         <div className={s.container}>
-            
+           <input type={"text"} placeholder={"Title"}/>
+           <div className={s.editor}>
+              <button className={s.button}>
+                  <Image src={"/plus-circle.svg"} alt={"add content"} width={16} height={16}/>
+              </button>
+               {open && <div className={s.add}>
+                   <button className={s.button}>
+                       <Image src={"/plus-circle.svg"} alt={"add content"} width={16} height={16}/>
+                   </button>
+                   <button className={s.button}>
+                       <Image src={"/external.svg"} alt={"add external content"} width={16} height={16}/>
+                   </button>
+                   <button className={s.button}>
+                       <Image src={"/video.svg"} alt={"add video"} width={16} height={16}/>
+                   </button>
+               </div>}
+           </div>
         </div>
     )
 }
