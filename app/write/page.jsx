6eb6b8d7b5a8292239d10d528acdd930129444db
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-13 16:25:29 +0200

    start writing page

---
+++ app/write/page.jsx
@@ -0,0 +1,11 @@
+import s from "./writePage.module.css";
+
+const WritePage = () => {
+    return (
+        <div className={s.container}>
+            
+        </div>
+    )
+}
+
+export default WritePage;
