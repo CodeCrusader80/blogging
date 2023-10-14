8a2d3eaa5de80bca372aee947d479cdd31a77415
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-14 16:38:58 +0200

    Writing section advance

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -3,20 +3,23 @@
 import s from "./writePage.module.css";
 import Image from "next/image";
 import {useState} from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
 
 const WritePage = () => {
 
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState("");
     return (
         <div className={s.container}>
            <input type={"text"} placeholder={"Title"}/>
            <div className={s.editor}>
-              <button className={s.button}>
+              <button className={s.button} onClick={() => setOpen(!open)}>
                   <Image src={"/plus-circle.svg"} alt={"add content"} width={16} height={16}/>
               </button>
-               {open && <div className={s.add}>
+               {open && (<div className={s.add}>
                    <button className={s.addButton}>
-                       <Image src={"/plus-circle.svg"} alt={"add content"} width={16} height={16}/>
+                       <Image src={"/image.svg"} alt={"add content"} width={16} height={16}/>
                    </button>
                    <button className={s.addButton}>
                        <Image src={"/external.svg"} alt={"add external content"} width={16} height={16}/>
@@ -24,7 +27,9 @@ const WritePage = () => {
                    <button className={s.addButton}>
                        <Image src={"/video.svg"} alt={"add video"} width={16} height={16}/>
                    </button>
-               </div>}
+               </div>
+               )}
+               <ReactQuill theme={"bubble"} value={value} onChange={setValue} placeholder={"Tell your story..."}/>
            </div>
         </div>
     )
