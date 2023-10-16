97caf98b1488ab7fe08e00ae75bcf18d39cb05eb
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-16 16:04:41 +0200

    Start create auth from Google

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,16 @@
+'use client'
 import s from "./loginPage.module.css";
+import {useSession, signIn} from "next-auth/react";
 
 const LoginPage = () => {
+
+    const {data, status } = useSession();
+
+    console.log(data, status);
     return (
         <div className={s.container}>
             <div className={s.wrapper}>
-             <div className={s.socialButton}>Sign in with Google</div>
+             <div className={s.socialButton} onClick={() => signIn("google")}>Sign in with Google</div>
              <div className={s.socialButton}>Sign in with GitHub</div>
              <div className={s.socialButton}>Sign in with Facebook</div>
             </div>
--- app/utils/auth.js
+++ app/utils/auth.js
@@ -4,8 +4,8 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
         }),
         GithubProvider({
             clientId: process.env.GITHUB_ID,
