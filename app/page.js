7aedc12cb05442aa7364fee037afafe5ace5af2a
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-21 17:45:47 +0200

    database association for posts

--- app/page.js
+++ app/page.js
@@ -5,13 +5,15 @@ import CategoryList from "@/app/src/components/categoryList/CategoryList";
 import CardList from "@/app/src/components/cardList/CardList";
 import Menu from "@/app/src/components/menu/Menu";
 
-export default function Home() {
+export default function Home({searchParams}) {
+
+const page = parseInt(searchParams.page) || 1;
   return (
     <div className={styles.container}>
       <Featured/>
       <CategoryList/>
       <div className={styles.content}>
-          <CardList/>
+          <CardList page={page}/>
           <Menu/>
       </div>
     </div>
--- app/src/components/cardList/CardList.jsx
+++ app/src/components/cardList/CardList.jsx
@@ -3,7 +3,20 @@ import s from "./cardlist.module.css";
 import Pagination from "@/app/src/components/pagination/Pagination";
 import Image from "next/image";
 import Card from "@/app/src/components/card/Card";
-const CardList = () => {
+
+const getData = async () => {
+    const res = await fetch("http://localhost:3000/api/posts", {
+        cache: "no-store",
+    });
+
+    if(!res.ok){
+        throw new Error("Failed")
+    }
+    return res.json()
+};
+const CardList = async() => {
+
+    const data = await getData()
     return(
         <div className={s.container}>
             <h1 className={s.title}>Recent Posts</h1>
