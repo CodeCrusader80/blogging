ead0a8854a5ee8dea1f8252ea4002d5777c61f43
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-22 15:16:48 +0200

    Update route.js

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -3,11 +3,20 @@ import prisma from "@/app/src/utils/connect";
 
 export const GET = async (req)=> {
 
+    const {searchParams} = new URL(req.url)
+
+    const page = searchParams.get("page")
+
     const POST_PER_PAGE = 2;
 
     try{
 
-      const posts = await prisma.post.findMany()
+      const posts = await prisma.post.findMany(
+          {
+              take: POST_PER_PAGE,
+              skip: POST_PER_PAGE * (page - 1),
+          }
+      );
 
         return new NextResponse(JSON.stringify(posts, {status: 200}))
 
