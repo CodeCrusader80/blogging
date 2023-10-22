5c3c390d77cbae105151243a15f9b3e8ee0a51d0
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-22 21:09:24 +0200

    pagination work
    now the user cannot navigate to a page below or above the number of articles actually present on the platform

--- app/src/components/cardList/CardList.jsx
+++ app/src/components/cardList/CardList.jsx
@@ -16,16 +16,20 @@ const getData = async (page) => {
 };
 const CardList = async({page}) => {
 
-    const data = await getData(page)
+    const {posts, count} = await getData(page)
+
+    const POST_PER_PAGE = 2;
+    const hasPrev = POST_PER_PAGE * (page-1) > 0;
+    const hasNext = POST_PER_PAGE * (page-1) + POST_PER_PAGE < count;
     return(
         <div className={s.container}>
             <h1 className={s.title}>Recent Posts</h1>
             <div className={s.posts}>
-                {data?.map((item) => (
+                {posts?.map((item) => (
                     <Card item={item} key={item._id}/>
                     ))}
             </div>
-        <Pagination page={page}/>
+        <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext}/>
         </div>
     )
 }
--- app/src/components/pagination/Pagination.jsx
+++ app/src/components/pagination/Pagination.jsx
@@ -3,13 +3,21 @@
 import React from "react";
 import s from "./pagination.module.css";
 import {useRouter} from "next/navigation";
-const Pagination = ({page}) => {
+const Pagination = ({page, hasPrev, hasNext}) => {
 
     const router = useRouter()
     return(
         <div className={s.container}>
-            <button className={s.button} onClick={()=>router.push(`?page=${page - 1}`)}>Previous</button>
-            <button className={s.button} onClick={()=>router.push(`?page=${page + 1}`)}>Next</button>
+            <button
+                className={s.button}
+                onClick={()=>router.push(`?page=${page - 1}`)}
+                disabled={!hasPrev}
+            >Previous</button>
+            <button
+                className={s.button}
+                onClick={()=>router.push(`?page=${page + 1}`)}
+                disabled={!hasNext}
+            >Next</button>
         </div>
     )
 }
