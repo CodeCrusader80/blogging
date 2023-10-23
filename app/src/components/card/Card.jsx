9797b59f11dd910c350544c1b23927d36d1f7ca5
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-23 17:56:22 +0200

    some changes for redirect

--- app/src/components/card/Card.jsx
+++ app/src/components/card/Card.jsx
@@ -6,18 +6,20 @@ const Card = ({key, item}) => {
     return (
         <div className={s.container} key={key}>
             <div className={s.imgContainer}>
-                <Image src={"/p2.png"} alt={"photo of a woman in a cyberpunk universe"} width={350} height={280} className={s.image}/>
+                {item.img && <Image src={item.img} alt={"photo of a woman in a cyberpunk universe"} className={s.image}/>}
             </div>
             <div className={s.txtContainer}>
                 <div className={s.detail}>
                     <span className={s.date}>{item.createAt.substring(0, 10)} - </span>
                     <span className={s.cat}>{item.catSlug}</span>
                 </div>
-                <Link href={"/page"}>
+                <Link href={`/posts/${item.slug}`}>
                 <h1 className={s.title}>{item.title}</h1>
                 </Link>
-                <p className={s.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cumque delectus, deserunt illo porro tenetur...</p>
-                <Link href={"/"} className={s.link}>Read More</Link>
+                <p className={s.desc}>
+                    {item.desc.substring(0, 60)}
+                </p>
+                <Link href={`/posts/${item.slug}`} className={s.link}>Read More</Link>
             </div>
         </div>
     )
