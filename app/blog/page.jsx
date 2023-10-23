5452f9f6cd25ce851dba7d0f3b426fa4fe053cbe
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-23 17:35:36 +0200

    start cat link with database

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,12 +2,16 @@ import s from "./blogPage.module.css";
 import CardList from "@/app/src/components/cardList/CardList";
 import Menu from "@/app/src/components/menu/Menu";
 
-const BlogPage = () => {
+const BlogPage = ({searchParams}) => {
+
+    const page = parseInt(searchParams.page) || 1;
+    const {cat} = searchParams;
+
     return(
         <div className={s.container}>
-            <h1 className={s.title}>Blog</h1>
+            <h1 className={s.title}>{cat}</h1>
             <div className={s.content}>
-                <CardList/>
+                <CardList page={page} cat={cat}/>
                 <Menu/>
             </div>
         </div>
