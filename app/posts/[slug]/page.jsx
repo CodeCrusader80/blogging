a1fa693b3ebd3d5fa28ea9213494aa75b4b0976d
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-24 15:53:27 +0200

    Update page.jsx

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -3,16 +3,30 @@ import Menu from "@/app/src/components/menu/Menu";
 import Image from "next/image";
 import Comments from "@/app/src/components/comments/Comments";
 
-const SinglePage = () => {
+const getData = async (slug) => {
+    const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+        cache: "no-store",
+    });
+
+    if(!res.ok){
+        throw new Error("Failed")
+    }
+    return res.json()
+};
+
+const SinglePage = async ({params}) => {
+
+    const {slug} = params;
+    const data = await getData(slug)
     return (
         <div className={s.container}>
             <div className={s.infoContainer}>
                 <div className={s.txtContainer}>
-                    <h1 className={s.title}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                    <h1 className={s.title}>{data?.title}</h1>
                     <div className={s.user}>
-                        <div className={s.userImgContainer}>
-                            <Image src={"/p2.png"} alt={""} fill className={s.userImg}/>
-                        </div>
+                        {data?.img && <div className={s.userImgContainer}>
+                            <Image src={data.img} alt={""} fill className={s.userImg}/>
+                        </div>}
                         <div className={s.userTxtContainer}>
                             <span className={s.username}>John Doe</span>
                             <span className={s.date}>11/10/2023</span>
