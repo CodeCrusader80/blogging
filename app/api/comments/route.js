65652b24ec87616b5dfa4e38cd5cad43a52ffc36
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-26 11:23:41 +0200

    add function to create a comment by user

--- app/api/comments/route.js
+++ app/api/comments/route.js
@@ -1,5 +1,6 @@
 import {NextResponse} from "next/server";
 import prisma from "@/app/src/utils/connect";
+import {getAuthSession} from "@/app/src/utils/auth";
 
 //GET THE COMMENTS OF A POST
 export const GET = async (req)=> {
@@ -11,6 +12,28 @@ try {
            where: {
                ...(postSlug && {postSlug}),
            },
+            include: {user: true},
+        });
+
+
+        return new NextResponse(JSON.stringify(comments, {status: 200}))
+
+    } catch (error) {
+        console.log(error)
+        return new NextResponse(JSON.stringify({message: "Something went wrong"}, {status: 500}))
+    }
+}
+
+//CREATE A COMMENT
+export const POST = async (req)=> {
+    const session = getAuthSession();
+    const postSlug = searchParams.get("postSlug");
+    try {
+
+        const comments = await prisma.comment.findMany({
+            where: {
+                ...(postSlug && {postSlug}),
+            },
             include: {user: true},
         });
 
